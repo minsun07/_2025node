e40373337478d5ff86c65f14cebc563c7d97d481
Minsun An <elleto123456@gmail.com>
2025-03-19 11:36:59 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+// json 형태의 데이터를 받으면 처리하겠다는 의미
+app.use(express.json());
+
 // 라우팅 설정
 app.post('/swag', (req, res) => {
   // 클라이언트에게 응답 내용 전송
-  res.send('Hello, Node!');
+  res.send(req.body);
 });
 
 
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.')
-});
+});  
