c0df8c16d2f5b0190108059ea21d0f8e260d104d
Minsun An <elleto123456@gmail.com>
2025-03-19 11:49:24 +0900

    param 넘기기

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 
 // 라우팅 설정
-app.post('/swag', (req, res) => {
-  // 클라이언트에게 응답 내용 전송
-  res.send(req.body);
+app.get('/swag/:person', (req, res) => {
+  const persons = req.params.person;
+  res.status(200).send(persons);
 });
 
 
