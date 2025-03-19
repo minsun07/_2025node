dd660a03723fec3b59ab3d06c96f1a70b0ba94b5
Minsun An <elleto123456@gmail.com>
2025-03-19 12:19:26 +0900

    Router 적용 후

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ const app = express();
 // json 형태의 데이터를 받으면 처리하겠다는 의미
 app.use(express.json());
 
+// 라우팅 파일 불러오기
+const swagRoutes = require('./routes/swag');
+
 // 라우팅 설정
-app.get('/swag', (req, res) => {
-  res.status(200).send('Get swag');
-});
-
-app.post('/swag', (req, res) => {
-  res.status(200).send('Post swag');
-});
-
-app.get('/swag/:person', (req, res) => {
-  const persons = req.params.person;
-  res.status(200).send(persons);
-});
-
+app.use('/swag', swagRoutes);
 
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.')
